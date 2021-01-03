92b6575c8dbac2629adc405f1a45baba3f8e855e
Gurveen <gurveens11@gmail.com>
2021-01-03 12:13:08 +0530

    update port value

--- app.js
+++ app.js
@@ -5,22 +5,21 @@ var bodyParser = require("body-parser");
 
 app.set("view engine", "ejs");
 
-app.get('/', function(req,res){
-	res.render("search");
+app.get("/", function (req, res) {
+  res.render("search");
 });
 
-app.get('/results', function(req,res){
-	var search = req.query.search;
-	var url = 'http://www.omdbapi.com/?s='+search+'&apikey=thewdb'; 
-    request(url, function(error,response,body){
-       if(!error && response.statusCode == 200){
-       	var data = JSON.parse(body);
-       	res.render("results", {data: data});
-       }
-   });                             
+app.get("/results", function (req, res) {
+  var search = req.query.search;
+  var url = "http://www.omdbapi.com/?s=" + search + "&apikey=thewdb";
+  request(url, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var data = JSON.parse(body);
+      res.render("results", { data: data });
+    }
+  });
 });
 
-
-app.listen(3000, function(){
-	console.log("Server has Started");
+app.listen(process.env.PORT || 3000, function () {
+  console.log("Server has Started");
 });
